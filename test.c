700742c1adf4f5eda6e1b261fccdd7d6932d7c4f
Your Name <avinashmn1999@gmail.com>
2023-04-23 17:34:59 +0200

    Adding LED blink for STM32

---
+++ test.c
@@ -0,0 +1,19 @@
+#include "stm32f4xx.h"
+
+void delay(uint32_t time) {
+  while(time--) {
+    __NOP();
+  }
+}
+
+int main(void) {
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // enable GPIOD clock
+  GPIOD->MODER |= GPIO_MODER_MODE15_0; // set PD15 as output
+
+  while(1) {
+    GPIOD->ODR |= GPIO_ODR_OD15; // set PD15 high
+    delay(1000000); // wait for 1s
+    GPIOD->ODR &= ~GPIO_ODR_OD15; // set PD15 low
+    delay(1000000); // wait for 1s
+  }
+}
